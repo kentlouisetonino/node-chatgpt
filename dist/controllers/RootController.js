66e24e8e4bd380ebc54fdb6c93a3a0f659d07b3b
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-03-11 02:07:51 +0800

    Remove the dist.

--- dist/controllers/RootController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class RootController {
-    static home(_, res) {
-        return res.sendFile('index.html', { root: 'public' });
-    }
-}
-exports.default = RootController;
